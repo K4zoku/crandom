7d3cdcf0ba296e7dc33e55b60d490b8370feb36a
Kazoku <k4zoku@pm.me>
2024-11-28 12:12:22 +0700

    feat(random_device.posix.c): token check

--- src/random_device.posix.c
+++ src/random_device.posix.c
@@ -2,6 +2,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#include <assert.h>
+#include <string.h>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -22,18 +27,43 @@ void random_device_dtor(random_device_t *instance) {
   free(instance);
 }
 
+static bool streq(const char * s1, const char * s2) {
+  return strcmp(s1, s2) == 0;
+}
+
+static const char * SUPPORTED_TOKEN[] = {
+  "/dev/urandom",
+  "/dev/random",
+};
+
 random_device_t *random_device_ctor_token(const char *token) {
-  random_device_t *rd = (random_device_t *)malloc(sizeof(random_device_t));
-  rd->fd = open(token, O_RDONLY);
-  if (rd->fd == -1) {
-    free(rd);
+  bool is_default = token == NULL || token[0] == '\0' || streq(token, "default");
+  if (is_default) {
+    token = "/dev/urandom";
+  } else {
+    bool supported = false;
+    for (size_t i = 0; i < sizeof(SUPPORTED_TOKEN) / sizeof(SUPPORTED_TOKEN[0]); i++) {
+        if (streq(token, SUPPORTED_TOKEN[i])) {
+            supported = true;
+        }
+    }
+    if (!supported) {
+        perror("random_device_ctor_token: unsupported token");
+        exit(1);
+        return NULL;
+    }
+  }
+  int fd = open(token, O_RDONLY);
+  if (fd == -1) {
     return NULL;
   }
+  random_device_t *rd = (random_device_t *)malloc(sizeof(random_device_t));
+  rd->fd = fd;
   return rd;
 }
 
 random_device_t *random_device_ctor(void) {
-  return random_device_ctor_token("/dev/urandom");
+  return random_device_ctor_token(NULL);
 }
 
 #endif /* _WIN32 */
